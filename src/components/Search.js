50fcf0766f12f5a84e02d9d1ff52f7e2fa35f896
Andrii Matviichuk <matviichuk.andr@gmail.com>
2020-11-21 08:11:48 +0100

    small update

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,12 @@
 import SearchInput from "./SearchInput";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function Search() {
   return (
     <div className="Search">
       <SearchInput />
+      <FontAwesomeIcon className="search-icon 2x" icon={faSearch} size="3x" />
     </div>
   );
 }
